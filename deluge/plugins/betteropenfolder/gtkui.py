184c2a2befd44aee96a1feda17e3e7f18675d26e
XrXr <xrsquared@gmail.com>
2016-02-14 21:58:16 -0500

    Basic open in Thunar and options

---
+++ deluge/plugins/betteropenfolder/gtkui.py
@@ -0,0 +1,124 @@
+#
+# gtkui.py
+#
+# Copyright (C) 2016 Alan Wu <xrsquared@gmail.com>
+#
+# Basic plugin template created by:
+# Copyright (C) 2008 Martijn Voncken <mvoncken@gmail.com>
+# Copyright (C) 2007-2009 Andrew Resch <andrewresch@gmail.com>
+# Copyright (C) 2009 Damien Churchill <damoxc@gmail.com>
+# Copyright (C) 2010 Pedro Algarvio <pedro@algarvio.me>
+#
+# Deluge is free software.
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# deluge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with deluge.    If not, write to:
+# 	The Free Software Foundation, Inc.,
+# 	51 Franklin Street, Fifth Floor
+# 	Boston, MA  02110-1301, USA.
+#
+#    In addition, as a special exception, the copyright holders give
+#    permission to link the code of portions of this program with the OpenSSL
+#    library.
+#    You must obey the GNU General Public License in all respects for all of
+#    the code used other than OpenSSL. If you modify file(s) with this
+#    exception, you may extend this exception to your version of the file(s),
+#    but you are not obligated to do so. If you do not wish to do so, delete
+#    this exception statement from your version. If you delete this exception
+#    statement from all source files in the program, then also delete it here.
+#
+
+import gtk
+import logging
+import dbus
+import dbus.service
+
+from deluge.ui.client import client
+from deluge.plugins.pluginbase import GtkPluginBase
+import deluge.component as component
+import deluge.common
+import pkg_resources
+import os.path
+
+from common import get_resource
+
+log = logging.getLogger(__name__)
+
+class GtkUI(GtkPluginBase):
+    def enable(self):
+        self.glade = gtk.glade.XML(get_resource("config.glade"))
+
+        component.get("Preferences").add_page("Better Open Folder", self.glade.get_widget("config_frame"))
+        component.get("PluginManager").register_hook("on_apply_prefs", self.on_apply_prefs)
+        component.get("PluginManager").register_hook("on_show_prefs", self.on_show_prefs)
+
+        bus = dbus.SessionBus()
+        xfce_file_manager_object = bus.get_object('org.xfce.FileManager', '/org/xfce/FileManager')
+        self.xfce_file_manager = dbus.Interface(xfce_file_manager_object, 'org.xfce.FileManager')
+
+
+        self.menubar = component.get("MenuBar")
+        self.open_folder_widget = self.menubar.torrentmenu_glade.get_widget(
+            "menuitem_open_folder")
+
+        self.open_folder_widget.handler_block_by_func(
+            self.menubar.on_menuitem_open_folder_activate)
+
+        self.open_folder_handler_id = self.open_folder_widget.connect(
+            "activate", self._open_folder)
+
+    def _open_folder(self, data=None):
+        def _on_torrent_status(status):
+            folder, file = how_to_open(status["save_path"], status["files"])
+            timestamp = gtk.get_current_event_time()
+            self.xfce_file_manager.DisplayFolderAndSelect(folder, file, '', '')
+            # deluge.common.open_file(folder, timestamp=timestamp)
+        for torrent_id in component.get("TorrentView").get_selected_torrents():
+            component.get("SessionProxy").get_torrent_status(torrent_id, ["save_path", "files"]).addCallback(_on_torrent_status)
+
+    def disable(self):
+        self.open_folder_widget.handler_unblock_by_func(
+            menubar.on_menuitem_open_folder_activate)
+
+        self.open_folder_widget.disconnect_handler(self.open_folder_handler_id)
+
+
+        component.get("Preferences").remove_page("ThunarOpen")
+        component.get("PluginManager").deregister_hook("on_apply_prefs", self.on_apply_prefs)
+        component.get("PluginManager").deregister_hook("on_show_prefs", self.on_show_prefs)
+
+    def on_apply_prefs(self):
+        log.debug("applying prefs for ThunarOpen")
+
+        config = None
+        for btn in self.glade.get_widget("thunar").get_group():
+            if btn.get_active():
+                config = {
+                    "open_method": btn.get_name()
+                }
+                break
+        client.thunaropen.set_config(config)
+
+    def on_show_prefs(self):
+        client.thunaropen.get_config().addCallback(self.cb_pref_get_config)
+
+    def cb_pref_get_config(self, config):
+        "callback for on show_prefs"
+        log.info(config)
+        self.glade.get_widget(config["open_method"]).set_active(True)
+
+def how_to_open(save_path, files):
+    first_path = files[0]["path"]
+    if len(files) == 1:
+        return (save_path, first_path)
+    return os.path.join(save_path, os.path.dirname(first_path)), "."
